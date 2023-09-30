5b2832843d8a4e78f113f572ee6ed95f83dc775a
clem-kay <ckoomson75@gmail.com>
2023-09-30 20:05:42 +0000

    backend setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1')
+
+ // setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Service booking  API')
+    .setDescription('API documentation for Service booking app')
+    .setVersion('1.0')
+    .addTag('service booking')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // handle all user input validation globally
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(5000);
 }
 bootstrap();
